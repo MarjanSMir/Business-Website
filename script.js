fdb6bc5d98d5b373e7cb744a4e4e34305e5d324a
MarjansMir <marjansmirmohammad@outlook.com>
2022-08-03 19:49:13 +0430

    3 August

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
- function myfunction(){
+function myfunction(){
     var x = document.getElementById("menu");
     x.style.transition = "0.75s"
     if(x.style.top == "-1000%"){
-        x.style.top ="100%"
+        x.style.top ="100%";
     }else{
         x.style.top = "-1000%"
     }
