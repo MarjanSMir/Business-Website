91e91e375a23a31ab17bac69e81678163f1993a0
MarjansMir <marjansmirmohammad@outlook.com>
2022-08-09 14:48:10 +0430

    9 August

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-/*function myfunction(){
-    var x = document.getElementById("menu");
-    x.style.transition = "0.75s"
-    if(x.style.top == "-1000%"){
-        x.style.top ="100%";
-    }else{
-        x.style.top = "-1000%"
-    }
-}*/
 const menu = document.querySelector(".nav-btn");
 const navbar = document.querySelector(".navbar");
 menu.addEventListener("click", ()=>{
