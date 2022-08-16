993cc45418d09929fdf109b11533a921a0a855da
MarjansMir <marjansmirmohammad@outlook.com>
2022-08-16 11:15:10 +0430

    16 August - 1

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ menu.addEventListener("click", ()=>{
     navbar.classList.toggle("change");
     menu.classList.toggle('change');
 });
+//section 3 cards
+var swiper = new Swiper(".myswiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+  });
